2bdbd57f309ce3ebbf91f177b340c9ed715c9605
andy mahendra <andy.mahendra@sti.co.id>
2026-02-19 13:17:45 +0700

    fix: exclude /uploads from middleware matcher to fix 404 in production

--- middleware.ts
+++ middleware.ts
@@ -51,8 +51,9 @@ export const config = {
          * - api (API routes)
          * - _next/static (static files)
          * - _next/image (image optimization files)
+         * - uploads (uploaded files)
          * - favicon.ico (favicon file)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        '/((?!api|_next/static|_next/image|uploads|favicon.ico).*)',
     ],
 };
